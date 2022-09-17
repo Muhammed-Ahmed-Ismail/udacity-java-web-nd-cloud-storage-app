caedf80472d70073c433b5afc876f83822ee7f79
Muhammed-Ismail <muhammadahmed.free@gmail.com>
2022-09-17 16:22:21 +0200

    note model and note mapper

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/mappers/NoteMapper.java
@@ -0,0 +1,25 @@
+package com.udacity.jwdnd.course1.cloudstorage.mappers;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.Note;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface NoteMapper {
+
+    @Select("SELECT * FROM NOTES WHERE userid = #{userId}")
+    Note[] getNotesByUserId(int userId);
+
+    @Select("SELECT * FROM NOTES WHERE noteid = #{noteId}")
+    Note getNoteById(int noteId);
+
+    @Insert("INSERT INTO NOTES (userid,notetitle,notedescription) VALUES (#{userId},#{noteTitle},#{noteDescription})")
+    @Options(useGeneratedKeys = true, keyProperty = "noteId")
+    int insertNote(Note note);
+
+    @Update("UPDATE NOTES SET notetitle=#{noteTitle} , noteDescription = #{noteDescription} WHERE noteid = #{noteId}")
+    void updateNote(Note note);
+
+    @Delete("DELETE FROM NOTES WHERE noteid = #{noteId}")
+    void deleteNote(Note note);
+
+}
